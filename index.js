85c0b35c584bc0cd976c1e702df82d44a86f0c4d
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2024-07-30 15:29:32 +0530

    removed the section which was not needed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
@@ -12,9 +13,6 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
-app.get("/profile/:username/:age", (req, res) => {
-    res.send(`You requested the profile of ${req.params.username} who is ${req.params.age} years old`);
-});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
