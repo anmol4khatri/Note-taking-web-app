1734fe202aa9ddd2e35f527208ac504a9f42e32d
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2024-07-30 17:56:35 +0530

    added "Create Note" functionality

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render("index");
+    fs.readdir('./files', (err, files) => {
+        res.render("index", {files: files});
+    });
+});
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.tittle.split(' ').join('_')}.txt`, req.body.details, (err) => {
+       res.redirect('/'); 
+    });
 });
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`app is listening at http://localhost:${port}`);
 }); 
